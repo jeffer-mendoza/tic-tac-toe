433a4e36e968ace47da8dcb397f02931dc07d572
jefferson <mendosajefferson@gmail.com>
2019-09-05 00:07:52 -0500

    Updates

---
+++ precache-manifest.a2ec188d2ef7098ff53ddf9a1db67fa4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "285f43401aa4c9f671bfce2f98d6a2a6",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "2d45bccbc26a72b98d4d",
+    "url": "/tic-tac-toe/static/css/main.56fa5a11.chunk.css"
+  },
+  {
+    "revision": "28374adeaaf19fa5403b",
+    "url": "/tic-tac-toe/static/js/2.147b0a47.chunk.js"
+  },
+  {
+    "revision": "2d45bccbc26a72b98d4d",
+    "url": "/tic-tac-toe/static/js/main.e0c080cc.chunk.js"
+  },
+  {
+    "revision": "9d58961b7fe13681cd75",
+    "url": "/tic-tac-toe/static/js/runtime~main.c09b2e37.js"
+  }
+]);
